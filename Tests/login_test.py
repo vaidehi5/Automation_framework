ca94bac32f5bee9ef350557d6a615e235cce6e1b
vaidehi5 <vaidehi.deshpande05@gmail.com>
2020-05-11 15:09:50 +0530

    whitespaces removed

--- Tests/login_test.py
+++ Tests/login_test.py
@@ -2,7 +2,6 @@
 from selenium import webdriver
 import pytest
 
-
 class TestLogin():
     @pytest.fixture(scope="session")  #scope = session means browser will open once perform all test cases and than will get close
     def test_setup(self):
@@ -15,11 +14,8 @@ class TestLogin():
         driver.quit()
         print("test completed")
 
-
-
     def test_login(self, test_setup):
         driver.get("https://opensource-demo.orangehrmlive.com/")
-
         driver.find_element_by_id("txtUsername").send_keys("Admin")
         driver.find_element_by_id("txtPassword").send_keys("admin123")
         driver.find_element_by_id("btnLogin").click()
